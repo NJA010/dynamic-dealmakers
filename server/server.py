c2ef76e4706810438d3cf54fac601c5194347787
Niels Janssen <n.janssen1@portofrotterdam.com>
2024-03-15 16:32:53 +0100

    add get prices

--- server/server.py
+++ server/server.py
@@ -1,8 +1,9 @@
 # from dynamic_pricing.price import Price
-from dynamic_pricing.scrape import scrape
-
+from dynamic_pricing.scrape import scrape, get_requests_headers, api_key, audience
+from dynamic_pricing.model import get_simple_prices
 from flask import Flask
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 app = Flask(__name__)
@@ -17,6 +18,16 @@ app = Flask(__name__)
 def pricing():
     scrape()
     return "Data scraped!"
+
+
+@app.route('/prices', methods=['GET'])
+def get_prices():
+    headers = get_requests_headers(api_key, audience)
+    product_data = requests.get(f"{audience}/products", headers=headers).json()
+
+    result = get_simple_prices(product_data, low=3, high=100)
+    return result
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
