21e3160f4c30e67e922e711eee34d7270b4bc462
Niels Janssen <n.janssen1@portofrotterdam.com>
2024-03-15 16:46:00 +0100

    add test for simple serve

---
+++ tests/test_serve.py
@@ -0,0 +1,21 @@
+import pytest
+from dynamic_pricing.model import get_simple_prices
+import json
+from copy import deepcopy
+
+
+@pytest.fixture()
+def products():
+    # Opening JSON file
+    f = open('./data/products_format_example.json')
+    data = json.load(f)
+    return data
+
+
+def test_get_simple_prices(products):
+    prices = get_simple_prices(products, 1, 1)
+    result = deepcopy(products)
+    for product_name in result.keys():
+        for uuid in result[product_name]:
+            result[product_name][uuid] = 1.
+    assert prices == result
