9b60d4c6b210507932b909b2a16739748511d69f
Gerard Draadjer <g.draadjer@fetimgroup.com>
2024-03-22 16:53:43 +0100

    WIP: creating new tables to retrieve purchase info

--- server/src/dynamic_pricing/sql/init.sql
+++ server/src/dynamic_pricing/sql/init.sql
@@ -22,13 +22,6 @@ CREATE TABLE IF NOT EXISTS raw_stocks (
     payload JSONB NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS served_prices(
-    product_type VARCHAR(255) NOT NULL,
-    product_uuid VARCHAR(255) NOT NULL,
-    batch_id VARCHAR(255) NOT NULL,
-    price FLOAT NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS products (
     id INTEGER NOT NULL,
     scraped_at TIMESTAMP NOT NULL,
@@ -55,4 +48,37 @@ CREATE TABLE IF NOT EXISTS stocks (
     stock_amount INTEGER NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS stocks (
+    id INTEGER NOT NULL,
+    scraped_at TIMESTAMP NOT NULL,
+    stock_update_id INTEGER NOT NULL,
+    batch_id INTEGER NOT NULL,
+    stock_amount INTEGER NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS served_prices (
+    product_type VARCHAR(255) NOT NULL,
+    product_uuid VARCHAR(255) NOT NULL,
+    batch_id VARCHAR(255) NOT NULL,
+    price FLOAT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS stock_changes (
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    batch_name VARCHAR(255) NOT NULL,
+    batch_id VARCHAR(255) NOT NULL,
+    batch_price NUMERIC NOT NULL,
+    stock_difference INTEGER NOT NULL
+    revenue NUMERIC NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS our_scraped_prices (
+    id SERIAL PRIMARY KEY,
+    scraped_at TIMESTAMP NOT NULL,
+    batch_name VARCHAR(255) NOT NULL,
+    batch_id VARCHAR(255) NOT NULL,
+    price NUMERIC NOT NULL
+    request_source VARCHAR(255) NOT NULL
+    status_code INTEGER NOT NULL
+);
--- server/src/dynamic_pricing/sql/prices.sql
+++ server/src/dynamic_pricing/sql/prices.sql
@@ -1,28 +1,31 @@
 INSERT INTO prices
-WITH grouped_prices AS (
-  SELECT
-    id,
-    scraped_at,
-    p.key AS product_name,
-    jsonb_object_agg(pp.key, pp.value) AS batch_info
-  FROM
-    raw_prices,
-    jsonb_each(payload) AS p(key, value),
-    jsonb_each(p.value) AS pp(key, value)
-  WHERE id > {{ max_id }}
-  GROUP BY 1, 2, 3
-),
 
-prices AS (
-  SELECT
-    id,
-    scraped_at,
-    product_name,
-    p.key AS batch_name,
-    p.value AS competitor_prices
-  FROM
-    grouped_prices,
-    jsonb_each(batch_info) AS p(key, value)
+WITH (
+  grouped_prices AS (
+    SELECT
+      id,
+      scraped_at,
+      p.key AS product_name,
+      jsonb_object_agg(pp.key, pp.value) AS batch_info
+    FROM
+      raw_prices,
+      jsonb_each(payload) AS p(key, value),
+      jsonb_each(p.value) AS pp(key, value)
+    WHERE id > {{ max_id }}
+    GROUP BY 1, 2, 3
+  ),
+
+  prices AS (
+    SELECT
+      id,
+      scraped_at,
+      product_name,
+      p.key AS batch_name,
+      p.value AS competitor_prices
+    FROM
+      grouped_prices,
+      jsonb_each(batch_info) AS p(key, value)
+  )
 )
 
 SELECT
