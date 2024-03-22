02d8309848ce5dce50075d9e5aef8b4d295fe678
Niels Janssen <n.janssen1@portofrotterdam.com>
2024-03-22 14:23:17 +0100

    update for scipy optimize

--- server/src/dynamic_pricing/model/revenue.py
+++ server/src/dynamic_pricing/model/revenue.py
@@ -1,15 +1,17 @@
 import numpy as np
+from dynamic_pricing.model.price_function import price_function_sigmoid
+from dynamic_pricing.utils import product_index
 
-
-def revenue_calc(data, price_func, **kwargs) -> float:
+def revenue_calc(data, x0: np.ndarray, **kwargs) -> float:
     """
     Given a price function, calculate what the revenue would have been for price data
     :param data: past price data
-    :param price_func: function to calculate the price
+    :param x0: (n,1) vector with parameters. needed for scipy optimize n=len(products)*n_parameters
     :param kwargs: additional parameters of price func
     :return: the revenue
     """
-    data["pred_price"] = price_func(data.quantity.values[:, None], **kwargs)
+    param_matrix = x0.reshape()
+    data["pred_price"] = price_function_sigmoid(data.quantity.values[:, None], **kwargs)
     data["sold"] = np.where(
         (data["pred_price"] <= data["sell_price"]) & (data["pred_price"] > 0), 1, 0
     )
