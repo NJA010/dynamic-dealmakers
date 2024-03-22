db0bd1928cc7b525cce6b759bbc0ade576ed243c
Niels Janssen <n.janssen1@portofrotterdam.com>
2024-03-22 13:47:14 +0100

    fix the list

--- tests/test_serve.py
+++ tests/test_serve.py
@@ -1,15 +1,27 @@
 import pytest
+
+from dynamic_pricing.database import DatabaseClient, load_config
 from dynamic_pricing.model.price_function import get_simple_prices
 import json
 from copy import deepcopy
+
+from dynamic_pricing.utils import get_stock
 
 
 @pytest.fixture()
 def products():
     # Opening JSON file
-    f = open('./tests/products_format_example.json')
+    f = open('./tests/product_data.json')
     data = json.load(f)
-    return data
+    return data[0][2]
+
+
+@pytest.fixture()
+def products_table():
+    # Opening JSON file
+    client = DatabaseClient(load_config())
+    stock = get_stock(client)
+    return stock
 
 
 def test_get_simple_prices(products):
